3e03ced77d47cf56957e4fd3c162928480cf0341
hukai <huzhengen@163.com>
2020-11-10 11:03:55 +0800

    Suspense

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <Suspense>
+    <template #default>
+      <AsyncShow />
+    </template>
+    <template #fallback>
+      <h1>Loading...</h1>
+    </template>
+  </Suspense>
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   <BasicExample />
   <Time />
   <Dog />
-  <Modal />
+  <!-- <Modal /> -->
 </template>
 
 <script lang="ts">
@@ -13,7 +21,8 @@ import HelloWorld from "./components/HelloWorld.vue";
 import BasicExample from "./components/BasicExample.vue";
 import Time from "./components/Time.vue";
 import Dog from "./components/Dog.vue";
-import Modal from "./components/Modal.vue";
+// import Modal from "./components/Modal.vue";
+import AsyncShow from "./components/AsyncShow.vue";
 
 export default defineComponent({
   name: "App",
@@ -22,7 +31,8 @@ export default defineComponent({
     BasicExample,
     Time,
     Dog,
-    Modal,
+    // Modal,
+    AsyncShow,
   },
 });
 </script>
