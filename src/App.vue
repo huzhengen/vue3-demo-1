c0a7da44ed91312edb437f0578013bf5f30c3689
hukai <huzhengen@163.com>
2020-11-10 10:48:25 +0800

    teleport

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  <BasicExample/>
-  <Time/>
-  <Dog/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <BasicExample />
+  <Time />
+  <Dog />
+  <Modal />
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-import BasicExample from './components/BasicExample.vue';
-import Time from './components/Time.vue';
-import Dog from './components/Dog.vue';
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import BasicExample from "./components/BasicExample.vue";
+import Time from "./components/Time.vue";
+import Dog from "./components/Dog.vue";
+import Modal from "./components/Modal.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
     BasicExample,
     Time,
     Dog,
-  }
+    Modal,
+  },
 });
 </script>
 
---
+++ src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+  <teleport to="body">
+    <div id="center">
+      <h2>modal-teleport</h2>
+    </div>
+  </teleport>
+</template>
+<script lang="ts">
+export default {};
+</script>
+<style>
+#center {
+  width: 200px;
+  height: 200px;
+  border: 2px solid black;
+  background: white;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate3d(-50%, -50%, 0);
+}
+</style>
